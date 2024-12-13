a087559cf33e6a532dcd11f4246388e7c911663a
Nitesh kumar Mandal <140904850+NiteshKrMandal@users.noreply.github.com>
2024-12-13 21:52:54 +0530

    Add files via upload
    Simple Cin task

---
+++ 2nd LAB/qn1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char name[30];
+    string nam;
+    cout<<"Enter Your Name\n";
+    cin.get(name,20);
+    cout<<"Hello\t"<<name<<endl;
+    return 0;
+}
+
+
