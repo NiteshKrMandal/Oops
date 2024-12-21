ca153c706fa2d588fc4762624e0d56b4567a0699
Nitesh kumar Mandal <140904850+NiteshKrMandal@users.noreply.github.com>
2024-12-21 21:51:25 +0530

    Add files via upload

---
+++ 6th LAB/qn1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class name{
+    private:
+    char name[20];
+    int roll;
+    int age;
+    char course[10];
+    public:
+    void getdata(){
+        cout<<"Enter Name"<<endl;
+        cin>>name;
+        cout<<"Roll no"<<endl;
+        cin>>roll;
+         cout<<"Age"<<endl;
+        cin>>age;
+         cout<<"Course"<<endl;
+        cin>>course;
+         
+    }
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Roll No: "<<roll<<endl;
+        cout<<"Age : "<<age<<endl;
+        cout<<"Course: "<<course<<endl;
+    }
+};
+class test:public name
+{
+    private:
+    int marks[4];
+    public:
+    void getdata()
+    {
+        cout<<"Enter 5 Subject marks"<<endl;
+        cin>>marks[0]>>marks[1]>>marks[2]>>marks[3]>>marks[4];
+    }
+    void display(){
+        cout<<"Marks In 5 subject are:"<<marks[0]<< "\t" <<marks[1] <<marks[2] <<marks[3]<<marks[4]<<endl;
+
+    }
+
+};
+int main(){
+    test t;
+    t.name::getdata();
+    t.getdata();
+    t.name::display();
+    t.display();
+}
