a88b1047853f46b85c230dd8c1583fd74b990532
Nitesh kumar Mandal <140904850+NiteshKrMandal@users.noreply.github.com>
2024-12-23 12:00:00 +0530

    Add files via upload

---
+++ 9th LAB/qn2.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+class employee{
+    protected:
+    char name[20];
+    int id;
+    float salary;
+    public:
+    void getdata()
+    {
+        cout<<"Enter Name of Employee: ";
+        cin>>name;
+        cout<<"Enter ID: ";
+        cin>>id;
+       // cout<<"Salary";
+        
+    }
+
+virtual void display()
+{
+    cout<<"Name: "<<name<<endl;
+    cout<<"ID: "<<id<<endl;
+   // cout<<"Salary: "<<salary()<<endl;
+}
+
+    
+};
+class regular: public employee
+{
+    int basic,da,hra;
+    public:
+    void getdata()
+    {
+    //employee::getdata();
+    cout<<"Enter Basic Salary , HRA , DA "<<endl;
+    cin>>basic>>hra>>da;
+    } 
+    // int salary1()
+    //     {
+    //         int basic,hra,da,q;
+    //         q=basic + hra + da;
+    //         return q;
+    //     }
+    void display()
+    {
+        //employee::display();
+        cout<<"Basic Salary: "<<basic<<endl;
+        cout<<"HRA: "<<hra<<endl;
+        cout<<"DA:"<<da<<endl;
+        cout<<"Salary: "<<(basic + hra + da)<<endl;
+    } 
+};
+class parttime: public employee
+{
+    int no,pay;
+    public:
+    void getdata()
+    {
+        //employee::getdata();
+        cout<<"Enter No of Hours: ";
+        cin>>no;
+        cout<<"Enter Pay Per Hour: ";
+        cin>>pay;
+    }
+    
+    void display()
+    {
+        //employee::display();
+        cout<<"No of Hours:"<<no<<endl;
+        cout<<"Pay Per Hour:"<<pay<<endl;
+        cout<<"Salary:"<<(no*pay)<<endl;
+    }
+};
+int main()
+{ employee *e1,*e2;
+   regular r;
+e1=&r;
+e1->getdata();
+e1->display();
+    parttime p;
+    e2=&p;
+    e2->getdata();
+    e2->display();
+  //  p.getdata();
+
+ //   p.display();
+}
